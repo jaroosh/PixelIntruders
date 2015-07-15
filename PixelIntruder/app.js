c6298c01f80878393735fd257f8233dfa4c57c67
Jarek Wawro <jarek.wawro@gmail.com>
2015-07-15 18:08:38 +0200

    Initial commit

---
+++ PixelIntruder/app.js
@@ -0,0 +1,24 @@
+﻿// Main game.
+var Game;
+(function (Game) {
+    var ArmadaGame = (function () {
+        function ArmadaGame() {
+            this.game = new Phaser.Game(1280, 720, Phaser.AUTO, 'content', {
+                create: this.create,
+                preload: this.preload
+            });
+        }
+        ArmadaGame.prototype.create = function () {
+        };
+
+        ArmadaGame.prototype.preload = function () {
+        };
+        return ArmadaGame;
+    })();
+    Game.ArmadaGame = ArmadaGame;
+
+    window.onload = function () {
+        var game = new ArmadaGame();
+    };
+})(Game || (Game = {}));
+//# sourceMappingURL=app.js.map
---
+++ PixelIntruder/app.ts
@@ -0,0 +1,23 @@
+﻿// Main game.
+module Game {
+    export class ArmadaGame {
+        game: Phaser.Game;
+
+        constructor() {
+            this.game = new Phaser.Game(1280, 720, Phaser.AUTO, 'content', {
+                create: this.create,
+                preload: this.preload
+            });
+        }
+
+        create() {
+        }
+
+        preload() {         
+        }
+    }
+
+    window.onload = () => {
+        var game = new ArmadaGame();
+    };
+}
